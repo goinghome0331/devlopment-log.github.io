08c1ea7a123352a9cf9f02f49a1b27b43d457d59
kangmingyu <goinghome0331@gmail.com>
2021-03-07 16:51:54 +0900

    post update:2021/03/07 Java Lamda

--- js/post.js
+++ js/post.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
     $('.card-text > a').click((event)=>{
         event.preventDefault();
         var link = event.target.href.split('#');
-        $('body').animate({scrollTop:$('#'+link[1]).offset().top - $('#'+link[1]).outerHeight()-10},500);
+        $('body').animate({scrollTop:$('#'+link[1]).offset().top - $('#'+link[1]).outerHeight()-30},500);
     });
 });
